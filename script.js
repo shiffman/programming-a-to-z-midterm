5783c873ad495f60edb15f568a88a12809190ef7
Rebecca Lieberman <rebeccalieberman@gmail.com>
2015-10-18 19:21:26 -0400

    added lots of stuff, figuring out how to query google image search API multiple times by using a loop

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ var input;
 var bttn;
 var searchTerm;
 
+var startNum = 1; //keeps track of where we are starting so we can make multiple API calls
+
+var allURLS = []; //create an empty array that will hold all the image URLs
+
 
 function setup() {
   noCanvas();
@@ -25,12 +29,20 @@ function imageSearch(){
 
   var my_key = 'AIzaSyBdeOzPYln1iX1B4lJrgeBFbrqzsx8Cj2A';
 
-  var query = "https://www.googleapis.com/customsearch/v1?key=" + my_key + "&searchType=image" + "&cx=017113430126644414771:wwhvz3sxr2q" + "&q=" + searchTerm  + "&num=10&start=1&imgSize=large";
-  var url = encodeURI(query); //encode url if you get multiple search terms
+  
+  //we need to make 10 different API calls (to get 100 images)
+  //this loop will first make us 10 different urls and then call loadJSON 10 times
+  //startNum will increment by 10 each time
 
-  loadJSON(url, gotData);
+  for (var i=0; i<10; i++) { 
+    var query = "https://www.googleapis.com/customsearch/v1?key=" + my_key + "&searchType=image&imgSize=large&imgType=photo" +  "&cx=017113430126644414771:wwhvz3sxr2q" + "&q=" + searchTerm  + "&num=10&start=" + startNum + "&imgSize=large";
+    var url = encodeURI(query); //encode url if you get multiple search terms
+    // console.log(url);
 
-//https://www.googleapis.com/customsearch/v1?q=drone&cx=017113430126644414771%3Awwhvz3sxr2q&imgSize=large&imgType=photo&num=20&searchType=image&key={YOUR_API_KEY}
+    startNum = startNum+10;
+    loadJSON(url, gotData); //make 10 calls to the API, each with a different URL
+
+  }
 
 
 
@@ -40,7 +52,22 @@ function imageSearch(){
 
 function gotData(data){
 
-  console.log(data);
+  var imgInfo = data.items; //imgInfo array of objects -- each image is an object with metadata
+
+  //for every image, grab the link from the object print out all the links
+  for (var i = 0; i < imgInfo.length; i++) {
+
+     var imgURL = imgInfo[i].link; //create a variable to hold the image URL
+
+     createP(imgURL);
+     var imgThumbnail = createImg(imgURL);
+     imgThumbnail.size(100,100);
+
+     allURLS.push(imgURL);  //store all the URLs in that array
+  }
+  
+  console.log(allURLS);
+
 
 }
 
